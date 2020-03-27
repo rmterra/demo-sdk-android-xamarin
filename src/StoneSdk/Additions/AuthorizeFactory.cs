d072768bf6eb220388ba423fe9fa8e165c06c6ef
rmterra <r.mirandaterra@gmail.com>
2020-03-27 10:11:30 -0300

    Import stone-sdk package with needed binding configurations

---
+++ src/StoneSdk/Additions/AuthorizeFactory.cs
@@ -0,0 +1,25 @@
+﻿using Android.Runtime;
+using Java.Interop;
+using System;
+
+namespace Stone.Environment.Factory
+{
+	public sealed partial class AuthorizeFactory
+	{
+		[Register("getEnvironment", "(Lstone/environment/Environment;)Lstone/environment/interfaces/AuthorizerEnvironment;", "GetGetEnvironment_Lstone_environment_Environment_Handler")]
+		public override unsafe Interfaces.IEnvironmentServices GetEnvironment(Environment environment)
+		{
+			const string __id = "getEnvironment.(Lstone/environment/Environment;)Lstone/environment/interfaces/AuthorizerEnvironment;";
+			try
+			{
+				JniArgumentValue* __args = stackalloc JniArgumentValue[1];
+				__args[0] = new JniArgumentValue((environment == null) ? IntPtr.Zero : environment.Handle);
+				var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod(__id, this, __args);
+				return GetObject<Interfaces.AuthorizerEnvironment>(__rm.Handle, JniHandleOwnership.TransferLocalRef);
+			}
+			finally
+			{
+			}
+		}
+	}
+}
---
+++ src/StoneSdk/Additions/PinpadObject.cs
@@ -0,0 +1,25 @@
+﻿using Android.Runtime;
+using Java.Interop;
+using System;
+
+namespace Stone.Utils
+{
+	public partial class PinpadObject
+	{
+		[Register("compareTo", "(Lstone/utils/PinpadObject;)I", "GetCompareTo_Lstone_utils_PinpadObject_Handler")]
+		public unsafe int CompareTo(Java.Lang.Object otherPinpad)
+		{
+			const string __id = "compareTo.(Lstone/utils/PinpadObject;)I";
+			try
+			{
+				JniArgumentValue* __args = stackalloc JniArgumentValue[1];
+				__args[0] = new JniArgumentValue((otherPinpad == null) ? IntPtr.Zero : otherPinpad.Handle);
+				var __rm = _members.InstanceMethods.InvokeVirtualInt32Method(__id, this, __args);
+				return __rm;
+			}
+			finally
+			{
+			}
+		}
+	}
+}
